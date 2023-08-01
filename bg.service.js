2f302d21831ea92407b497602d5d952faab7547b
陈 润 <godakid@outlook.com>
2023-08-01 14:01:02 +0800

    chore: style

--- bg.service.js
+++ bg.service.js
@@ -1,2 +1,13 @@
 // service-worker.js
+// All it does is add a listener for the push event, create the payload variable consisting of the text taken from the data (or create a string to use if data is empty)
+self.addEventListener("push", (event) => {
+    const payload = event.data?.text() ?? "no payload";
+    event.waitUntil(
+        self.registration.showNotification("ServiceWorker Cookbook", {
+            body: payload,
+        }),
+    );
+});
+
+
 
